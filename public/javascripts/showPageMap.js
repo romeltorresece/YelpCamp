9faa81dd84784cc1f96ba7c8fd23379ab071684b
Romel Torres <romeltorres.ece@gmail.com>
2022-05-15 13:08:47 +0800

    YelpCamp: Styles Clean Up

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,8 +3,10 @@ const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/satellite-streets-v11', // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
-  zoom: 10 // starting zoom
+  zoom: 12 // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 const marker = new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
